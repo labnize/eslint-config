8b682efd21e94a39cc87ccd925832211ae8727f2
July <july@JulydeMacBook-Pro.local>
2017-07-15 14:46:28 +0800

    Initial

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  env: {
+    browser: true
+  },
+  rules: {
+    quotes: [2, 'single'],
+    semi: 2,
+    'max-len': [1, 120, 2],
+    'arrow-body-style': [1, 'as-needed'],
+    'comma-dangle': [2, 'never'],
+    'no-debugger': 2,
+    'no-console': 2,
+    'object-curly-spacing': [2, 'always'],
+    'no-undef': [1],
+    'new-cap': 1,
+    'no-param-reassign': 2,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'global-require': 0
+  }
+};
